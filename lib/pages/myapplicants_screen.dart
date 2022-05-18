d461c929cdfccdf5915822cf94d3b98b99b0e973
Harun Nurahman <harun.nurrahman@hotmail.com>
2022-05-18 11:03:44 +0700

    Layouting My Applicants Screen

--- lib/pages/myapplicants_screen.dart
+++ lib/pages/myapplicants_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ghawejobapp/shared/jobboxes.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MyApplicantScreen extends StatelessWidget {
   const MyApplicantScreen({Key? key}) : super(key: key);
@@ -6,8 +8,41 @@ class MyApplicantScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('My Applicant Screen'),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text(
+          'Pekerjaan Tersimpan'.toUpperCase(),
+          style: GoogleFonts.poppins(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const <Widget>[
+              JobBoxes(
+                logoComp: 'assets/images/img_gojek.png',
+                jobDesk: 'Social Media Engineer',
+                company: 'GoJek',
+                location: 'Jakarta, Indonesia',
+              ),
+              SizedBox(height: 8),
+              JobBoxes(
+                logoComp: 'assets/images/img_tokped.png',
+                jobDesk: 'Full Stack Developer',
+                company: 'Tokopedia',
+                location: 'Bandung, Indonesia',
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
