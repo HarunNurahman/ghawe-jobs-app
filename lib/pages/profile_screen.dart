b5632dc276dd16712158060b96ab47566f5b7569
Harun Nurahman <harun.nurrahman@hotmail.com>
2022-05-18 13:15:14 +0700

    Layouting Profile Screen

--- lib/pages/profile_screen.dart
+++ lib/pages/profile_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ghawejobapp/shared/config.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({Key? key}) : super(key: key);
@@ -6,8 +8,103 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Profile Screen'),
+      backgroundColor: Colors.white,
+      appBar: appBar(),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            children: [
+              Column(
+                children: [
+                  const Center(
+                    child: CircleAvatar(
+                      radius: 64,
+                      backgroundColor: kPrimaryColor,
+                      child: CircleAvatar(
+                        radius: 60,
+                        backgroundColor: Colors.white,
+                        child: CircleAvatar(
+                          radius: 56,
+                          backgroundImage:
+                              AssetImage('assets/images/img_avatar.jpg'),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  Text(
+                    'Harun Nurahman',
+                    style: GoogleFonts.poppins(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  const SizedBox(height: 4),
+                  Text(
+                    'Junior Mobile Developer',
+                    style: GoogleFonts.poppins(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Icon(
+                        Icons.location_on,
+                        color: kPrimaryColor,
+                        size: 8,
+                      ),
+                      const SizedBox(width: 6),
+                      Text(
+                        'Kota Bandung, Jawa Barat',
+                        style: GoogleFonts.poppins(
+                          fontSize: 10,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 4),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Icon(
+                        Icons.phone,
+                        color: kPrimaryColor,
+                        size: 8,
+                      ),
+                      const SizedBox(width: 6),
+                      Text(
+                        'xxxx-xxxx-xxxx',
+                        style: GoogleFonts.poppins(
+                          fontSize: 10,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      centerTitle: true,
+      backgroundColor: Colors.white,
+      elevation: 0,
+      title: Text(
+        'Profil Saya'.toUpperCase(),
+        style: GoogleFonts.poppins(
+          color: Colors.black,
+          fontWeight: FontWeight.bold,
+        ),
       ),
     );
   }
